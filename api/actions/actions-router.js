d51cbd9e030236439a61f22db1db6dbfcfede32b
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-09 10:33:09 -0600

    wires actions-router  to server and endpoints function with placeholder console logs

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,32 @@
 // Write your "actions" router here!
+const express = require('express')
+const Actions = require('./actions-model')
+
+const router = express.Router()
+
+// [GET] fetches array of actions
+router.get('/', (req, res) => {
+    console.log('get is great success')
+})
+
+// [GET] fetches action with given id
+router.get('/:id', (req, res) => {
+    console.log('get action by id is great success')
+})
+
+// [POST] creates and returns new action
+router.post('/', (req, res) => {
+    console.log('post is great success')
+})
+
+// [PUT] updates action with given id and returns updated action
+router.put('/:id', (req, res) => {
+    console.log('put is great success')
+})
+
+// [DELETE] deletes action with given id and returns no response body
+router.delete('/:id', (req, res) => {
+    console.log('delete is great success')
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -5,8 +5,10 @@ const server = express();
 server.use(express.json())
 
 const projectsRouter = require('./projects/projects-router')
+const actionsRouter = require('./actions/actions-router')
 
 server.use('/api/projects', projectsRouter)
+server.use('/api/actions', actionsRouter)
 
 server.use(errorHandling)
 // Configure your server here
