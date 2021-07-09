2d6a4416b307afb076339668c4607bfed4fa5c8d
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-09 09:28:00 -0600

    configures server and wires projects-router endpoints to server - currently with placeholder console.logs for each endpoint

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,37 @@
 // Write your "projects" router here!
+const express = require('express')
+const Projects = require('./projects-model')
+
+const router = express.Router()
+
+// [GET] fetches array of projects
+router.get('/', (req, res) => {
+    console.log('get is great success')
+})
+
+// [GET] fetches project based on id
+router.get('/:id', (req, res) => {
+    console.log('get by id is great success')
+})
+
+// [POST] creates and returns new project
+router.post('/', (req, res) => {
+    console.log('post is great success')
+})
+
+// [PUT] updates project and returns updated project
+router.put('/:id', (req, res) => {
+    console.log('put is great success')
+})
+
+// [DELETE] deletes project and returns no response body
+router.delete('/:id', (req, res) => {
+    console.log('delete is great success')
+})
+
+// [GET] fetches array of action belonging to project with given id
+router.get('/:id/actions', (req, res) => {
+    console.log('get actions is great success')
+})
+
+module.exports = router
